4e1658f7c8db73b02383c28ab024c32583c9ac3f
unknown <pil717171@gmail.com>
2020-04-16 20:41:04 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+
+function initSlider() {
+    $(document).ready(function(){
+        $('.hero-content-slider').slick({
+            arrows: false,
+            slidesToShow: 2,
+        }
+        );
+    });
+}
+
+initSlider()
+
