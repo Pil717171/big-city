f9cc8b31b364f4f5b06080592b063b8952f59824
unknown <pil717171@gmail.com>
2020-04-28 18:14:29 +0300

    ADDED NAVIGATION

--- src/js/index.js
+++ src/js/index.js
@@ -50,8 +50,119 @@ function initExampleSlider () {
 
 }
 
+function initCommentsSlider() {
+    $('.comment-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        prevArrow: document.querySelector('.comment-slider-arrows-prev'),
+        nextArrow: document.querySelector('.comment-slider-arrows-next'),
+        }
+    );
+}
+
+window.addEventListener('load', function () {
+    siteNavigation();
+    
+});
+
+
+
+function siteNavigation() {
+    let count = 0;
+    let newCount;
+    let inProgress = false;
+    let sections = Array.prototype.slice.call(document.querySelectorAll('section'));
+    let maxCount = sections.length - 1;
+    let changeCountEvent = new Event('changeCount');
+    let menuSection = document.querySelector('.menu');
+    let menuItems = Array.prototype.slice.call(document.querySelectorAll('.menu-item'));
+
+
+
+    menuItems.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            count = item.getAttribute('data-num');
+            window.dispatchEvent(changeCountEvent);
+            menuSection.classList.remove('open')
+        })
+    })
+
+    window.addEventListener('changeCount', (e) => {
+        renderPage(count)
+    })
+
+    function renderPage (num) {
+        sections[num].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+    }
+
+    function changeCountToWhell () {
+        window.addEventListener('wheel', (e) => {
+            if(!inProgress) {
+                if(e.deltaY > 0) {
+                    (count < maxCount) ? count++ : count = maxCount;
+                    window.dispatchEvent(changeCountEvent)
+                    inProgress = true;
+                    changeProgress()
+                } else if (e.deltaY < 0) {
+                    (count > 0 && count <= maxCount) ? count-- : count = 0;
+                    window.dispatchEvent(changeCountEvent)
+                    inProgress = true;
+                    changeProgress()
+                }
+            }
+            
+        })
+    }
+
+    function changeCountToClick() {
+        let buttonsTop = Array.prototype.slice.call(document.querySelectorAll('.grid-counter-top'));
+        let buttonsBottom = Array.prototype.slice.call(document.querySelectorAll('.grid-counter-bottom'));
+        buttonsTop.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                (count > 0 && count <= maxCount) ? count-- : count = 0;
+                window.dispatchEvent(changeCountEvent)
+            })
+        })
+        buttonsBottom.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                (count < maxCount) ? count++ : count = maxCount;
+                window.dispatchEvent(changeCountEvent)
+            })
+        })
+    }
+
+    function changeProgress () {
+        setTimeout(function() {
+            inProgress = false
+        }, 1000)
+    }
+
+    function menuBehavior () {
+        let menuOpenButton = document.querySelectorAll('.hamburger');
+        menuOpenButton.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                menuSection.classList.toggle('open')
+            })
+        })
+    
+    }
+
+
+    menuBehavior()
+    renderPage(0)
+    changeCountToWhell()
+    changeCountToClick()
+    
+}
+
+
 
 
 initExampleSlider()
 initMainSlider()
+initCommentsSlider()
 
